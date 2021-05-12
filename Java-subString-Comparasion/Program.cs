4f7a555b8661b825a7e7beb3f3de7b5e146f75e4
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-12 03:09:07 -0500

    Agregar archivos de proyecto.

---
+++ Java-subString-Comparasion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java_subString_Comparasion
+{
+    class Program
+    {
+        static String primerYultimo(String s,int n)
+        {
+            String primero, ultimo;
+            ultimo = s.Substring((s.Length-n));
+            char[] first = s.ToCharArray();
+            Array.Reverse(first);
+            String m = new string(first);
+            primero = m.Substring((m.Length-n));
+            return primero + "\n"+ultimo;
+        }
+        static void Main(string[] args)
+        {
+           Console.WriteLine( primerYultimo("welcometojava",3));
+        }
+    }
+}
